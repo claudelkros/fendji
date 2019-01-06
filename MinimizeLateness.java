3bd6f589be8b33d5060ef1f2378bb89ef72ecb3b
claudelkros <desiretaira@gmail.com>
2019-01-06 23:08:44 +0100

    End of work

--- MinimizeLateness.java
+++ MinimizeLateness.java
@@ -5,27 +5,40 @@ import java.util.Arrays;
 class Job implements Comparable<Job>{
 	public String name;
 	public int processingTime;
-	public int deadline;
+	public int dueDate;
 	public int start;
 	public int finish;
+        public int release;
 
-	public Job(String name, int processingTime, int dueDate){
+
+	public Job(String name, int processingTime, int dueDate, int release){
 		this.name=name;
 		this.processingTime=processingTime;
-		this.deadline=dueDate;
+		this.dueDate=dueDate;
+                this.release = release;
 	}
 
 	public int compareTo(Job job) {
-		return this.deadline - job.deadline;
+          
+            if (this.release < job.release){ 
+                if (this.dueDate < job.dueDate) {
+                     return this.dueDate;
+                }
+                return job.dueDate;
+            }
+            return this.dueDate - job.dueDate;
+               
 	}
-
+        
+        
 	@Override
 	public String toString(){
-		return "[Job " + name + ":  Processing Time="+processingTime + ", due="+deadline + ",   (start="+start + ", finish="+finish + ")]";
+		return "[Job " + name + ":  Processing Time="+processingTime + ", due="+dueDate + ", release="+release + ",   (start="+start + ", finish="+finish + ")]";
 	}
 }
 
 public class MinimizeLateness {
+
 	public static void  findMinLateness(Job[] jobs){
 		Arrays.sort(jobs);		//Sort by earliest deadline
 
@@ -35,12 +48,13 @@ public class MinimizeLateness {
 			job.finish = currentIntervalEndTime + job.processingTime;
 			currentIntervalEndTime += job.processingTime;
 		}
+                
 
 		//Loop over jobs to find any that are late and find the maximum lateness value
 		int maxLateness=0;
 		String maxLateJob = "";		//Initialize to empty string in case no job is late
 		for(Job job : jobs){
-			int lateness = job.finish - job.deadline;	//Calculate lateness. Negative means it's NOT late
+			int lateness = job.finish - job.dueDate;	//Calculate lateness. Negative means it's NOT late
 			if(lateness > maxLateness){
 				maxLateness = lateness;
 				maxLateJob = job.name;
@@ -57,16 +71,22 @@ public class MinimizeLateness {
 		}
 	}
 
-
+        
 	public static void main(String[] args) {
 		Job[] jobs = {
-				new Job("1", 4, 8),
-				new Job("2", 2, 12),
-				new Job("3", 6, 11),
-				new Job("4", 5, 10),
+				/*new Job("1", 4, 8, 0),
+				new Job("2", 2, 12, 1),
+				new Job("3", 6, 11, 2),
+				new Job("4", 5, 10, 3), */
+                                new Job("1", 4, 8, 0),
+				new Job("2", 5, 10, 3),
+				new Job("3", 6, 11, 2),
+				new Job("4", 2, 12, 1), 
 				
 		};
 		MinimizeLateness.findMinLateness(jobs);
 	}
+
+
 
 }
